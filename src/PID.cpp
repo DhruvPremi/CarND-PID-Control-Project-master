3f777243d90ec72010c3f46bcb094941607d7927
DHRUV PREMI <dhruvpremi@gmail.com>
2018-06-01 07:39:44 +0530

    PID Values given. Car Working Perfectly

---
+++ src/PID.cpp
@@ -0,0 +1,41 @@
+#include "PID.h"
+
+using namespace std;
+
+/*
+* TODO: Complete the PID class.
+*/
+
+PID::PID() {}
+
+PID::~PID() {}
+
+void PID::Init(double Kp, double Ki, double Kd) 
+{
+
+	PID::Kp = Kp;
+	PID::Ki = Ki;
+	PID::Kd = Kd;
+
+	p_error = 0;
+  	i_error = 0;
+  	d_error = 0;
+
+  	last_cte = 0;
+
+}
+
+void PID::UpdateError(double cte) 
+{
+	p_error = cte;
+	d_error = cte - last_cte;
+	i_error += cte;
+
+	last_cte = cte;
+}
+
+double PID::TotalError() 
+{
+
+}
+
